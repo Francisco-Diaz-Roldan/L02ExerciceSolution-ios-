81e5709888b8ff4491a896c1207b2fddbd5b933b
Francisco Diaz Roldan <francisco.diazro@viewnet.com>
2024-03-15 05:37:03 -0700

    Initial Commit

---
+++ L02ExerciseSolution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L02ExerciseSolution
+//
+//  Created by Francisco Díaz Roldán on 15/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
